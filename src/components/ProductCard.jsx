375f990fcf0a482a2acb33ac7222e98d475d5480
Parekh_Harsh <harshparekh8503@gmail.com>
2026-04-16 20:34:41 +0530

    changed image size manully

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-export default function ProductCard({ title, image, price, link, tag }) {
+export default function ProductCard({ title, image, price, link, tag,imgClass }) {
   return (
     <div className="group relative rounded-2xl overflow-hidden border border-white/10 bg-white/5 backdrop-blur-md transition duration-300 hover:-translate-y-1 hover:shadow-xl hover:shadow-blue-500/10">
 
@@ -10,21 +10,21 @@ export default function ProductCard({ title, image, price, link, tag }) {
       )}
 
       {/* IMAGE */}
-      <div className="relative h-72 w-full overflow-hidden bg-[#111]">
+      <div className="relative h-[350px] w-full overflow-hidden bg-[#111]">
         <img
           src={image}
           alt={title}
-          className="w-full h-full object-cover object-top transition duration-500 group-hover:scale-105"
+          className={`w-full h-full object-cover object-top transition duration-500 group-hover:scale-105 ${imgClass}`}
         />
       </div>
 
       {/* CONTENT */}
       <div className="p-4">
-        <h3 className="text-sm font-semibold text-white line-clamp-2 min-h-[44px]">
+        <h3 className="text-sm font-semibold text-white  ">
           {title}
         </h3>
 
-        <p className="text-blue-400 font-bold mt-2 text-xl">
+        <p className="text-blue-400 font-bold mt-0 text-xl">
           {price}
         </p>
 
@@ -33,7 +33,7 @@ export default function ProductCard({ title, image, price, link, tag }) {
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="mt-4 block w-full text-center px-4 py-3 rounded-xl bg-blue-600 hover:bg-blue-700 transition font-semibold"
+          className="mt-4 block w-full text-center px-4 py-2 rounded-xl bg-blue-600 hover:bg-blue-700 transition font-semibold"
         >
           Buy Now
         </a>
--- src/data/fashion.js
+++ src/data/fashion.js
@@ -1,12 +1,14 @@
 const fashion = [
   {
-    id: 1,
-    title: "sky blue baggy jeans for men",
-    price: "₹899",
-    image: "/images/Fashion/Lightbluejeans.jpg",
-    link: "https://amazon.in",
-    tag: "Best Seller"
-  },
+  id: 1,
+  title: "sky blue baggy jeans for men",
+  price: "₹899",
+  image: "/images/Fashion/Lightbluejeans.jpg",
+  link: "https://amazon.in",
+  tag: "Best Seller",
+
+  imgClass: "object-cover object-top scale-110",
+},
   {
     id: 2,
     title: "Baggy Jeans for Men",
