234ad29c4ea25e7759bffdd99dd2b89fe1d223bb
Parekh_Harsh <harshparekh8503@gmail.com>
2026-04-17 20:14:51 +0530

    fashion data update

--- src/data/fashion.js
+++ src/data/fashion.js
@@ -39,6 +39,41 @@ const fashion = [
     image: "/images/Fashion/6251e298a76f0671a8c016a5a4f08f3f.jpg",
     link: "https://ajiio.in/54b78Uf",
   },
+  {
+    id: 6,
+    title: "🔥 Everyday Straight Fit Jeans You NEED 👖💙",
+    price: "₹860",
+    image: "/images/Fashion/5b4a1d736fbe00d3acbc3848e4d27a42.jpg",
+    link: "https://ajiio.in/D42sqJn",
+  },
+  {
+    id: 7,
+    title: "💙 Indigo Baggy Jeans 👖 | Streetwear Outfit for Men 🔥",
+    price: "₹1699",
+    image: "/images/Fashion/f43f21b8bfd08ecd4665d75b95b4d4a1.jpg",
+    link: "https://ajiio.in/66FdHIn",
+  },
+  {
+    id: 8,
+    title: "💙 Baggy Dark Wash Jeans 👖 | Streetwear Outfit for Men 🔥",
+    price: "2250",
+    image: "/images/Fashion/f5324b150d00018959f0880185da9c09.jpg",
+    link: "https://ajiio.in/4YNz9ft",
+  },
+  {
+    id: 9,
+    title: "🖤 Premium Casual Shirt 👔 | Smart Casual Outfit for Men 🔥✨",
+    price: "₹800",
+    image: "/images/Fashion/90b211b08292e3b644a09321e294d028.jpg",
+    link: "https://myntr.it/3eZsB3e",
+  },
+   {
+    id: 10,
+    title: "💙 High Rise Blue Jeans 👖 | Clean Streetwear Outfit for Men 🔥",
+    price: "1100",
+    image: "/images/Fashion/44f36ef1e3357496c7aa2994dc4d29d9.jpg",
+    link: "https://ajiio.in/GCThapi",
+  },
 ];
 
 export default fashion;
