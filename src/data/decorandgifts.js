7294841570d88acce8c36615d6dce44b05608f4a
Parekh_Harsh <harshparekh8503@gmail.com>
2026-04-19 14:54:02 +0530

    decor add

--- src/data/decorandgifts.js
+++ src/data/decorandgifts.js
@@ -1,36 +1,13 @@
 const decorandgifts = [
-  {
+ {
     id: 1,
-    title: "LED Moon Lamp",
-    price: "₹799",
-    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?auto=format&fit=crop&w=600&q=80",
-    link: "https://amazon.in",
-    tag: "Trending"
+    title: "Divine Radha Krishna LED Wall Art ✨ A Stunning Spiritual Home Decor Piece",
+    price: "₹300",
+    image: "/images/Decor/9ea07455ea0879025abd43c13e6be7c0.jpg",
+    link: "https://bitli.in/CPpp4BL",
+    imgClass: "object-[center_60%] "
+
   },
-  {
-    id: 2,
-    title: "Luxury Scented Candles",
-    price: "₹599",
-    image: "https://images.unsplash.com/photo-1602872029708-84d970d3380a?auto=format&fit=crop&w=600&q=80",
-    link: "https://amazon.in",
-    tag: "Best Seller"
-  },
-  {
-    id: 3,
-    title: "Photo Frame Gift Set",
-    price: "₹999",
-    image: "https://images.unsplash.com/photo-1513519245088-0e12902e5a38?auto=format&fit=crop&w=600&q=80",
-    link: "https://amazon.in",
-    tag: "Gift Pick"
-  },
-  {
-    id: 4,
-    title: "Artificial Flower Vase",
-    price: "₹699",
-    image: "https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b?auto=format&fit=crop&w=600&q=80",
-    link: "https://amazon.in",
-    tag: "Hot Deal"
-  }
 ];
 
 export default decorandgifts;
